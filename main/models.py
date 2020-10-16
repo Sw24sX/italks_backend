e4065b04cbe7873bd75f8aa90e5b9972413ed8dd
Sw24sX <56915392+Sw24sX@users.noreply.github.com>
2020-10-16 14:23:41 +0500

    Начал думать над передачей иконок вместе с категориями

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    icon = models.ImageField(upload_to='icons/', blank=True)
     objects = models.Manager()
 
     class Meta:
