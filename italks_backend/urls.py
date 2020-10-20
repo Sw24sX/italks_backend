29987e62010372f357bf0b1157f1573f36e7edac
Sw24sX <56915392+Sw24sX@users.noreply.github.com>
2020-10-20 17:08:02 +0500

    Добавил документацию. Для работы необходимо установить drf-yasg2

--- italks_backend/urls.py
+++ italks_backend/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('main.main_urls'))
 ]
+
+urlpatterns += doc_urls
