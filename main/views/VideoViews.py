dd71c39e703c0a59cc6de3b7200ce9ea0be2c7a1
Sw24sX <aleksandr.korolyov.99@mail.ru>
2020-12-04 12:24:43 +0500

    Добавил видео по пероидам в категории и подкатегории

--- main/views/VideoViews.py
+++ main/views/VideoViews.py
@@ -8,6 +8,9 @@ from ..models import Category, Subcategory, Video
 
 from ..serializers.VideoSerializer import VideoSerializer
 
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+
 
 class VideoViews(APIView):
     """Информация о видео"""
@@ -43,24 +46,62 @@ class VideoListCategoryView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, category_pk):
+        # todo добавить сортировку
+        period = request.query_params.get('period')
 
-        category = Category.objects.filter(pk=category_pk).first()
-        if category is None:
+        #category = Category.objects.filter(pk=category_pk).first()
+        #if category is None:
+        #    return Response(status=400)
+
+        subcategory_id = request.query_params.get('subcategory')
+
+        if period == "year":
+            # ONLY FOR DEV
+            pk_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            videos = Video.objects.filter(pk__in=pk_list, category__id=category_pk)
+            if subcategory_id is not None:
+                videos = videos.filter(subcategory__id=subcategory_id)
+
+            #videos = Video.objects.filter(date__lt=self.get_date_start_current_month()) \
+            #    .filter(date__gte=self.get_date_start_current_year())
+        elif period == "month":
+            pk_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+            videos = Video.objects.filter(pk__in=pk_list, category__id=category_pk)
+
+            if subcategory_id is not None:
+                videos = videos.filter(subcategory__id=subcategory_id)
+            #videos = Video.objects.filter(date__lt=self.get_date_start_week()) \
+            #    .filter(date__gte=self.get_date_start_current_month())
+        elif period == "week":
+            pk_list = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+            videos = Video.objects.filter(pk__in=pk_list, category__id=category_pk)
+
+            if subcategory_id is not None:
+                videos = videos.filter(subcategory__id=subcategory_id)
+            #videos = Video.objects.filter(date__gte=self.get_date_start_week())
+        else:
             return Response(status=400)
 
-        videos = Video.objects.filter(category=category)
+        # todo добавить обработку исключений; попробовать использовать annotate
+        #page_size = request.query_params.get('page_size')
+        page_size = 10
+        paginator = Paginator(videos, page_size)
+        page = request.query_params.get('page')
+        try:
+            videos = paginator.page(page)
+        except EmptyPage:
+            return Response(status=400)
+        except:
+            return Response(status=400)
+        serialized_result = VideoSerializer(videos, many=True)
+        data = {
+            "is_last_page": int(page) == paginator.num_pages,
+            "number_pages": paginator.num_pages,
+            "videos_page": serialized_result.data
+        }
+        return Response(data, status=201)
 
-        subcategories_name = request.query_params.getlist('subcategories', None)
-        if subcategories_name is not None and len(subcategories_name) != 0:
-            subcategories = Subcategory.objects.filter(name__in=subcategories_name)
-            videos = Video.objects.filter(subcategory__in=subcategories)
 
-        serializer = VideoSerializer(videos.distinct(), many=True)
-        return Response(serializer.data, status=201)
-
-
-from django.core.paginator import Paginator
-from django.core.paginator import EmptyPage
 
 class VideosViews(APIView):
     """Видео на главной"""
