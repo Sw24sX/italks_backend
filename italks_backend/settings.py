8398dd847fd9bdf0d4ca643d1f32b9015a26a70b
Sw24sX <aleksandr.korolyov.99@mail.ru>
2020-12-11 16:07:07 +0500

    изменил ссылку сброса пароля

--- italks_backend/settings.py
+++ italks_backend/settings.py
@@ -186,8 +186,8 @@ SITE_NAME = "itlaks"
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
-    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {
